87b9fa7cf06b99f14c039a97ee3ea4a9785f6e34
ARYAN GUPTA <911aaryan@gmail.com>
2025-08-09 10:22:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import Loader from "./components/Loader";
 
 
 
-
-
 const HomePage = lazy(() => import("./pages/Home"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Customers = lazy(() => import("./pages/Customers"));
