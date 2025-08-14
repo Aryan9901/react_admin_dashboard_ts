dd420d0ff44ed50acb6ae3fce1d39f034d15658d
ARYAN GUPTA <911aaryan@gmail.com>
2025-08-14 23:40:26 +0530

    Update LineCharts.tsx

--- src/pages/charts/LineCharts.tsx
+++ src/pages/charts/LineCharts.tsx
@@ -2,8 +2,6 @@ import AdminSidebar from "../../components/AdminSidebar";
 import { LineChart } from "../../components/Charts";
 
 
-
-
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 const LineCharts = () => {
