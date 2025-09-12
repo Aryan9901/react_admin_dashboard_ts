9580f96dbb79120126def8ab03e690467f6a51f7
ARYAN GUPTA <911aaryan@gmail.com>
2025-09-12 22:45:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
 
-
-
 const HomePage = lazy(() => import("./pages/Home"));
 const Dashboard = lazy(() => import("./pages/Dashboard"));
 const Customers = lazy(() => import("./pages/Customers"));
